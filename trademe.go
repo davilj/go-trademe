b5df4ef957e3d5a86f2da3312ac186cfbb88fdfa
Danie Viljoen <danie.viljoen@orionhealth.com>
2016-05-04 23:25:56 +1200

    more logging

--- trademe.go
+++ trademe.go
@@ -9,14 +9,16 @@ import (
 
 func main() {
 	p:=fmt.Println
-	p("Starting trademe")
+	p("Starting trademe..1")
 	dataDrive:="/trademeData"
 	if (len(os.Args)>1) {
+		p("configuring data drive")
 		dataDrive=os.Args[1]
 	}
 	p("Running daily summary on:", dataDrive )
 
 	var pgSummaryHandler trademelib.PostgresSummaryHandler;
 	trademelib.CalcDailySums(dirForDailySummary, pgSummaryHandler)
+	p("Completed....")
 
 }
